70dd90e0fb71b41371f560761e43bb30859deeb5
irvantaufik <irvantaufik28@gmail.com>
2024-01-03 11:23:19 +0700

    list test

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,14 +1,31 @@
 import express, { Router } from "express";
+import { logger } from "../application/logging";
 import serverless from "serverless-http";
+import cors from "cors"
+import { prismaClient } from "../application/database";
+
 
 const api = express();
+
+api.use(cors({
+    origin: "*",
+  }));
+  
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
+
+router.get('/list', async (req, res) => {
+    
+    const classmajor = await prismaClient.classMajor.findMany()
+
+    return res.status(200).json({ data: classmajor });
+})
+
 api.use("/api/", router);
 const PORT = process.env.PORT || 4000;
 api.listen(PORT, () => {
-    console.log(`App start at ${process.env.HOST}:${process.env.PORT}`);
+    logger.info(`App start at ${process.env.HOST}:${process.env.PORT}`);
   });
 export const handler = serverless(api);
